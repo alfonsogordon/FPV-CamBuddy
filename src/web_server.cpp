c3b54cd1132e0a44e08e9219e9d416e81ff6a77a
alfonsogordon <41699095+alfonsogordon@users.noreply.github.com>
2026-09-18 21:45:54 +0100

    Expose camera profile settings to AP configurator

--- src/web_server.cpp
+++ src/web_server.cpp
@@ -596,6 +596,10 @@ void WebConfigServer::handleGetConfig() {
     d["stop_on_disarm"] = c.stopOnDisarm;
     d["aux_channel"] = c.auxChannel;
     d["aux_mode"] = c.auxMode;
+    d["profile_aux"] = c.profileAuxChannel;
+    d["profile_low"] = c.profileLow;
+    d["profile_mid"] = c.profileMid;
+    d["profile_high"] = c.profileHigh;
     d["camera_match"] = c.cameraMatchMode;
     d["wake_guard"] = c.cameraWakeGuard;
     d["debug_ble"] = c.debugBle;
@@ -665,6 +669,10 @@ void WebConfigServer::handlePostConfig() {
     if (d["stop_on_disarm"].is<bool>()) _cfg->setStopOnDisarm(d["stop_on_disarm"].as<bool>());
     if (d["aux_channel"].is<int>()) _cfg->setAuxChannel(d["aux_channel"].as<uint8_t>());
     if (d["aux_mode"].is<int>()) _cfg->setAuxMode(d["aux_mode"].as<uint8_t>());
+    if (d["profile_aux"].is<int>()) _cfg->setProfileAuxChannel(d["profile_aux"].as<uint8_t>());
+    if (d["profile_low"].is<uint32_t>()) _cfg->setProfileId(0, d["profile_low"].as<uint32_t>());
+    if (d["profile_mid"].is<uint32_t>()) _cfg->setProfileId(1, d["profile_mid"].as<uint32_t>());
+    if (d["profile_high"].is<uint32_t>()) _cfg->setProfileId(2, d["profile_high"].as<uint32_t>());
     if (d["camera_match"].is<int>()) _cfg->setCameraMatchMode(d["camera_match"].as<uint8_t>());
     if (d["wake_guard"].is<bool>()) _cfg->setCameraWakeGuard(d["wake_guard"].as<bool>());
     if (d["debug_ble"].is<bool>()) _cfg->setDebugBle(d["debug_ble"].as<bool>());
