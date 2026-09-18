313635323944a5310ce71249bf690546fb5e199b
alfonsogordon <41699095+alfonsogordon@users.noreply.github.com>
2026-09-18 10:34:02 +0100

    Track AP save command diagnostics

--- src/web_server.h
+++ src/web_server.h
@@ -29,6 +29,9 @@ private:
     uint32_t        _lastProbeLogMs = 0;
     uint32_t        _lastProbeCountLogged = 0;
     bool            _firstHttpLogged = false;
+    uint16_t        _cliSetCount = 0;
+    uint32_t        _cliSaveStartMs = 0;
+    String          _cliKeys;
 
     void handleRoot();
     void handleGetConfig();
