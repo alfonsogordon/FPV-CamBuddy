6d22256cd44e03677d64f22a99822c0ebdb300f5
alfonsogordon <41699095+alfonsogordon@users.noreply.github.com>
2026-09-24 10:20:40 +0100

    V1.0.3: expose GPS time and advanced power in AP API

--- src/web_server.cpp
+++ src/web_server.cpp
@@ -622,6 +622,11 @@ void WebConfigServer::handleGetConfig() {
     d["wake_guard"] = c.cameraWakeGuard;
     d["debug_ble"] = c.debugBle;
     d["low_power"] = c.lowPowerMode;
+    d["advanced_power"] = c.advancedPowerMode;
+    d["advanced_dbm"] = c.advancedPowerDbm;
+    d["gopro_gps_time"] = c.goproGpsTimeSync;
+    d["gopro_tz_mode"] = c.goproTimezoneMode;
+    d["gopro_tz_offset"] = c.goproTimezoneOffsetMin;
     d["wifi_ap_enabled"] = c.wifiApEnabled;
     d["wifi_ap_delay"] = c.wifiApStartDelaySec;
     d["osd1"] = c.osd1Tpl;
@@ -699,6 +704,11 @@ void WebConfigServer::handlePostConfig() {
     if (d["wake_guard"].is<bool>()) _cfg->setCameraWakeGuard(d["wake_guard"].as<bool>());
     if (d["debug_ble"].is<bool>()) _cfg->setDebugBle(d["debug_ble"].as<bool>());
     if (d["low_power"].is<bool>()) _cfg->setLowPowerMode(d["low_power"].as<bool>());
+    if (d["advanced_power"].is<bool>()) _cfg->setAdvancedPowerMode(d["advanced_power"].as<bool>());
+    if (d["advanced_dbm"].is<int>()) _cfg->setAdvancedPowerDbm(d["advanced_dbm"].as<int8_t>());
+    if (d["gopro_gps_time"].is<bool>()) _cfg->setGoProGpsTimeSync(d["gopro_gps_time"].as<bool>());
+    if (d["gopro_tz_mode"].is<int>()) _cfg->setGoProTimezoneMode(d["gopro_tz_mode"].as<uint8_t>());
+    if (d["gopro_tz_offset"].is<int>()) _cfg->setGoProTimezoneOffsetMin(d["gopro_tz_offset"].as<int16_t>());
     if (d["wifi_ap_enabled"].is<bool>()) _cfg->setWifiApEnabled(d["wifi_ap_enabled"].as<bool>());
     if (d["wifi_ap_delay"].is<int>()) _cfg->setWifiApStartDelay(d["wifi_ap_delay"].as<uint32_t>());
     if (d["osd1"].is<const char*>()) _cfg->setOsdTemplate(1, d["osd1"].as<const char*>());
@@ -768,6 +778,11 @@ void WebConfigServer::handlePostConfig() {
     checkBool("wake_guard", c.cameraWakeGuard);
     checkBool("debug_ble", c.debugBle);
     checkBool("low_power", c.lowPowerMode);
+    checkBool("advanced_power", c.advancedPowerMode);
+    checkInt("advanced_dbm", c.advancedPowerDbm);
+    checkBool("gopro_gps_time", c.goproGpsTimeSync);
+    checkInt("gopro_tz_mode", c.goproTimezoneMode);
+    checkInt("gopro_tz_offset", c.goproTimezoneOffsetMin);
     checkBool("wifi_ap_enabled", c.wifiApEnabled);
     checkInt("wifi_ap_delay", c.wifiApStartDelaySec);
     checkStr("osd1", c.osd1Tpl);
