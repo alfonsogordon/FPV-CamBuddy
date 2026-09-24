20cc0092691469d0c40a423ce689f85b292a5173
alfonsogordon <41699095+alfonsogordon@users.noreply.github.com>
2026-09-24 10:21:24 +0100

    V1.0.3: apply advanced BLE power in insta360_camera.cpp

--- src/insta360_camera.cpp
+++ src/insta360_camera.cpp
@@ -4,6 +4,22 @@
 #include "ble_debug.h"
 #include <cctype>
 
+static esp_power_level_t selectedBlePower(bool advanced, int8_t dbm, bool low) {
+    if (!advanced) return low ? ESP_PWR_LVL_N12 : ESP_PWR_LVL_P9;
+    if (dbm <= -24) return ESP_PWR_LVL_N24;
+    if (dbm <= -21) return ESP_PWR_LVL_N21;
+    if (dbm <= -18) return ESP_PWR_LVL_N18;
+    if (dbm <= -15) return ESP_PWR_LVL_N15;
+    if (dbm <= -12) return ESP_PWR_LVL_N12;
+    if (dbm <= -9) return ESP_PWR_LVL_N9;
+    if (dbm <= -6) return ESP_PWR_LVL_N6;
+    if (dbm <= -3) return ESP_PWR_LVL_N3;
+    if (dbm <= 0) return ESP_PWR_LVL_N0;
+    if (dbm <= 3) return ESP_PWR_LVL_P3;
+    if (dbm <= 6) return ESP_PWR_LVL_P6;
+    return ESP_PWR_LVL_P9;
+}
+
 namespace {
 
 struct PbField {
@@ -129,7 +145,7 @@ Insta360Camera *Insta360Camera::_instance = nullptr;
 void Insta360Camera::begin() {
     _instance = this;
     BLEDevice::init("FreeCLinker");
-    BLEDevice::setPower(_lowPowerMode ? ESP_PWR_LVL_N12 : ESP_PWR_LVL_P9);
+    BLEDevice::setPower(selectedBlePower(_advancedPowerMode, _advancedPowerDbm, _lowPowerMode));
 
     startScan();
 }
